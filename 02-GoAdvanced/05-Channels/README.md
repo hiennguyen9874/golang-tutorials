dd3e221e3cd76d4fafc2f7d413defb5283aef24e
Xuan Hien <hiennguyn9874@gmail.com>
2023-02-27 16:44:12 +0700

    Channels

---
+++ 02-GoAdvanced/05-Channels/README.md
@@ -0,0 +1,21 @@
+# Channels
+
+Channels are the pipes that connect concurrent goroutines. You can send values into channels from one goroutine and receive those values into another goroutine
+
+- Create new channels
+- Send a value into a channel
+- Receive a value from the channel
+- Deadlock
+
+  ```go
+  package main
+
+  func main() {
+      ch := make(chan int)
+      ch <- 5
+  }
+  ```
+
+- Unidirectional channels
+- Closing channels
+- For range loops on channels
---
+++ 02-GoAdvanced/05-Channels/main.go
@@ -0,0 +1,63 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a chan int
+	if a == nil {
+		fmt.Println("channel a is nil, going to define it")
+		a = make(chan int)
+		fmt.Printf("Type of a is %T", a)
+	}
+
+	messages := make(chan string)
+
+	go func() {
+		// write to channel messages
+		messages <- "ping"
+	}()
+
+	// read from channel messages
+	msg := <-messages
+	fmt.Println(msg)
+
+	done := make(chan bool)
+	go func(done chan bool) {
+		fmt.Println("Hello world goroutines")
+		done <- true
+	}(done)
+	<-done
+	fmt.Println("Done hello world")
+
+	sendc := make(chan int)
+	go func(sendc chan<- int) {
+		sendc <- 10
+	}(sendc)
+	fmt.Println(<-sendc)
+
+	ch := make(chan int)
+	go func(chnl chan int) {
+		for i := 0; i < 10; i++ {
+			chnl <- i
+		}
+		close(chnl)
+	}(ch)
+	for {
+		v, ok := <-ch
+		if ok == false {
+			break
+		}
+		fmt.Println("Received: ", v, ok)
+	}
+
+	ch2 := make(chan int)
+	go func(chnl chan int) {
+		for i := 0; i < 10; i++ {
+			chnl <- i
+		}
+		close(chnl)
+	}(ch2)
+	for v := range ch2 {
+		fmt.Println("Received: ", v)
+	}
+}
