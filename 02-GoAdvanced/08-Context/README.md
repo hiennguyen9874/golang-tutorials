b33dceaa7ad90e548ef70b936d5189b5f2ee596d
Xuan Hien <hiennguyen9874@gmail.com>
2023-03-03 00:45:03 +0700

    Update Context

---
+++ 02-GoAdvanced/08-Context/README.md
@@ -0,0 +1,7 @@
+# Context
+
+- Create context
+- Using Data Within a Context
+- Ending a Context
+  - Determining if a Context is Done
+  - Canceling a Context
--- 02-GoAdvanced/08-Context/main.go
+++ 02-GoAdvanced/08-Context/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"time"
@@ -22,7 +23,106 @@ func hello(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func doSomething(ctx context.Context) {
+	fmt.Printf("doSomething: myKey's value is %s\n", ctx.Value("myKey"))
+
+	anotherCtx := context.WithValue(ctx, "myKey", "anotherValue")
+	doAnother(anotherCtx)
+
+	fmt.Printf("doSomething: myKey's value is %s\n", ctx.Value("myKey"))
+}
+
+func doAnother(ctx context.Context) {
+	fmt.Printf("doAnother: myKey's value is %s\n", ctx.Value("myKey"))
+}
+
+func doSomething2(ctx context.Context) {
+	ctx, cancelCtx := context.WithCancel(ctx)
+
+	printCh := make(chan int)
+	go doAnother2(ctx, printCh)
+
+	for num := 1; num <= 3; num++ {
+		printCh <- num
+	}
+
+	cancelCtx()
+
+	time.Sleep(100 * time.Millisecond)
+
+	fmt.Printf("doSomething: finished\n")
+}
+
+func doAnother2(ctx context.Context, printCh <-chan int) {
+	for {
+		select {
+		case <-ctx.Done():
+			if err := ctx.Err(); err != nil {
+				fmt.Printf("doAnother err: %s\n", err)
+			}
+			fmt.Printf("doAnother: finished\n")
+			return
+		case num := <-printCh:
+			fmt.Printf("doAnother: %d\n", num)
+		}
+	}
+}
+
+func doSomething3(ctx context.Context) {
+	deadline := time.Now().Add(1500 * time.Millisecond)
+	ctx, cancelCtx := context.WithDeadline(ctx, deadline)
+	defer cancelCtx()
+
+	printCh := make(chan int)
+	go doAnother2(ctx, printCh)
+
+	for num := 1; num <= 3; num++ {
+		select {
+		case printCh <- num:
+			time.Sleep(1 * time.Second)
+		case <-ctx.Done():
+			break
+		}
+	}
+
+	cancelCtx()
+
+	time.Sleep(100 * time.Millisecond)
+
+	fmt.Printf("doSomething: finished\n")
+}
+
+func doSomething4(ctx context.Context) {
+	ctx, cancelCtx := context.WithTimeout(ctx, 1500*time.Millisecond)
+	defer cancelCtx()
+
+	printCh := make(chan int)
+	go doAnother2(ctx, printCh)
+
+	for num := 1; num <= 3; num++ {
+		select {
+		case printCh <- num:
+			time.Sleep(1 * time.Second)
+		case <-ctx.Done():
+			break
+		}
+	}
+
+	cancelCtx()
+
+	time.Sleep(100 * time.Millisecond)
+
+	fmt.Printf("doSomething: finished\n")
+}
 func main() {
+	// Creating a Context
+	ctx := context.Background()
+	ctx = context.WithValue(ctx, "myKey", "myValue")
+	doSomething(ctx)
+	doSomething2(ctx)
+	doSomething3(ctx)
+	doSomething4(ctx)
+
 	http.HandleFunc("/hello", hello)
 	http.ListenAndServe(":8090", nil)
 }
